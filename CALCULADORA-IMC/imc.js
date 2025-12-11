e6835c106c49f5733e96edf81eeff9c38ece6134
Davi  Gomes <124318517+davigome@users.noreply.github.com>
2025-12-11 17:12:31 -0300

    NOVO PROJETO
    ADICIONANDO UM NOVO PROJETO

---
+++ CALCULADORA-IMC/imc.js
@@ -0,0 +1,39 @@
+
+const resultado = document.getElementById('resultado');
+
+function imc () {
+    const nome = document.getElementById('nome').value;
+    const altura = document.getElementById('altura').value;
+    const peso = document.getElementById('peso').value;
+    const calcular = document.getElementById('calcular');
+
+    if (nome !== '' && altura !== '' && peso !== '') {
+        
+        const valorIMC = (peso / (altura * altura)).toFixed(2);
+
+        let classificacao = "";
+
+        if (valorIMC < 18.5){
+            classificacao = 'Abaixo do peso.'
+        }else if (valorIMC < 25){
+            classificacao = 'Com peso ideal. Parabéns!!!'
+        }else if (valorIMC <30){
+            classificacao = 'Levemente acima do peso.'
+        }else if (valorIMC < 35){
+            classificacao = ' Com obesidade grau I.'
+        }else if (valorIMC < 40){
+            classificacao = 'Com obesidade grau II'
+        }else {
+            classificacao = 'Obesidade grau III. Cuidado!!!'
+        }
+
+        resultado.textContent = `${nome} seu IMC é ${valorIMC} e você está ${classificacao}`;
+
+    }else{
+        resultado.textContent = 'Preencha todos os campos!!!'
+
+    }
+
+}
+
+calcular.addEventListener('click', imc)
